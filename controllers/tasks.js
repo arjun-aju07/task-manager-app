379e7c04720de461691ed399e02382ef65681312
Arjun T <arjunaju.t@gmail.com>
2023-10-29 20:12:11 +0530

    added logic to get all tasks, get single task and delete task from DB

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,21 +1,77 @@
-const getAllTasks = (req, res) => {
-    res.send('Returns all the tasks')
+import Task from '../models/Task.js'
+
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({}) // will get all documents
+        res
+            .status(200)
+            .json({ tasks })
+    } catch (e) {
+        res
+            .status(500)
+            .json({ message: e })
+    }
 }
 
-const createTask = (req, res) => {
-    res.json(req.body)
+const createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body)
+        res
+            .status(201)
+            .json({ task })
+    } catch (e) {
+        res
+            .status(500)
+            .json({ message: e })
+    }
 }
 
-const getTask = (req, res) => {
-    res.send('Returns a specific task')
+const getTask = async (req, res) => {
+    const { id: taskId } = req.params
+
+    try {
+        const task = await Task.findOne({ _id: taskId }) // will return null if document doesn't exist
+
+        if(!task) {
+            return res
+                .status(404)
+                .json({ message: `No task found with ID ${taskId}` })
+        }
+
+        res
+            .status(200)
+            .json({ task })
+    } catch (e) {
+        res
+        .status(500)
+        .json({ message: e })
+    }
 }
 
 const updateTask = (req, res) => {
     res.send('Updates task')
 }
 
-const deleteTask = (req, res) => {
-    res.send('deletes task')
+const deleteTask = async (req, res) => {
+    const { id: taskId } = req.params
+
+    try {
+        const task = await Task.findOneAndDelete({ _id: taskId }) // will return null if document doesn't exist
+
+        if(!task) {
+            return res
+                .status(404)
+                .json({ message: `No task found with ID ${taskId}` })
+        }
+
+        res
+            .status(200)
+            .json({ task })
+    } catch (e) {
+        res
+            .status(500)
+            .json({ message: e })
+    }
 }
 
 export {
